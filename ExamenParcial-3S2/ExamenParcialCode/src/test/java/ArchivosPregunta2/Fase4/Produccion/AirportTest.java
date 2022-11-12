a782ba008b869608af99fed86bbc9b25ae86926e
DagmarLV <dagmar.lezama.v@uni.pe>
2022-11-11 23:37:46 -0500

    update ArchivosPregunta2

--- ExamenParcial-3S2/ExamenParcialCode/src/test/java/ArchivosPregunta2/Fase4/Produccion/AirportTest.java
+++ ExamenParcial-3S2/ExamenParcialCode/src/test/java/ArchivosPregunta2/Fase4/Produccion/AirportTest.java
@@ -12,18 +12,20 @@ public class AirportTest {
     @DisplayName("Dado que hay un vuelo economico")
     @Nested
     class EconomyFlightTest {
-
+        //declaramos las siguientes variables para que al momento de utilizar un nivel de prueba más
+        //se pueda reutilizar estos campos para todas las pruebas de esta clase
         private Flight economyFlight;
         private Passenger jessica;
         private Passenger cesar;
 
         @BeforeEach
+        //inicializamos las variables antes de la ejecución de cada prueba
         void setUp() {
             economyFlight = new EconomyFlight("1");
             jessica = new Passenger("Jessica", false);
             cesar = new Passenger("Cesar", true);
         }
-
+        //aca añadimos un nivel de prueba más para los 2 tipos de pasajeros: regulares y vip
         @Nested
         @DisplayName("Cuando tenemos un pasajero regular")
         class RegularPassenger {
@@ -31,6 +33,7 @@ public class AirportTest {
             @Test
             @DisplayName("Luego puede agregarlo y eliminarlo de un vuelo economico")
             public void testEconomyFlightRegularPassenger() {
+                //ahora se hace uso de un assertAll para probar todas condiciones
                 assertAll("Verifica todas las condiciones para un pasajero regular y un vuelo economico",
                         () -> assertEquals("1", economyFlight.getId()),
                         () -> assertEquals(true, economyFlight.addPassenger(jessica)),
@@ -41,7 +44,7 @@ public class AirportTest {
                 );
             }
         }
-
+    //De forma similar que en EconomyFlightTest
         @Nested
         @DisplayName("Cuando tenemos un pasajero VIP")
         class VipPassenger {
@@ -110,4 +113,55 @@ public class AirportTest {
     }
 
     // Completa la prueba para PremiumFlight de acuerdo a la logica comercial dada. Pregunta 8
+
+    @DisplayName("Dado que hay un vuelo premium")
+    @Nested
+    class PremiumTest {//declaramos la clase anidada para probar el tipo de vuelo premium
+        //en general, usaremos la misma lógica que se usó para implementar las anteriores
+        //clases anidadas
+        private Flight premiumFlight;
+        private Passenger jessica;
+        private Passenger cesar;
+        @BeforeEach
+        void setUp() {
+            premiumFlight = new PremiumFlight("3");
+            jessica = new Passenger("Jessica", false);
+            cesar = new Passenger("Cesar", true);
+        }
+        @Nested
+        @DisplayName("Cuando tenemos un pasajero regular")
+        class RegularPassenger {
+
+            @Test
+            @DisplayName("Entonces no puede agregarlo o eliminarlo de un vuelo premium")
+            public void testPremiumFlightRegularPassenger() {
+                assertAll("Verifica todas las condiciones para un pasajero regular y un vuelo premium",
+                        () -> assertEquals(false, premiumFlight.addPassenger(jessica)),
+                        () -> assertEquals(0, premiumFlight.getPassengersList().size()),
+                        () -> assertEquals(false, premiumFlight.removePassenger(jessica)),
+                        () -> assertEquals(0, premiumFlight.getPassengersList().size())
+                );
+            }
+        }
+
+        @Nested
+        @DisplayName("Cuando tenemos un pasajero VIP")
+        class VipPassenger {
+
+            @Test
+            @DisplayName("Luego puedes agregarlo pero no puedes eliminarlo de un vuelo de premium")
+            public void testPremiumFlightVipPassenger() {
+                assertAll("Verifica todas las condiciones para un pasajero VIP y un vuelo premium",
+                        () -> assertEquals(true, premiumFlight.addPassenger(cesar)),
+                        () -> assertEquals(1, premiumFlight.getPassengersList().size()),
+                        () -> assertEquals(false, premiumFlight.removePassenger(cesar)),
+                        () -> assertEquals(1, premiumFlight.getPassengersList().size())
+                );
+            }
+        }
+
+
+
+    }
+
 }
